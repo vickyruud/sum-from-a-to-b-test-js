e2dfd2d902c49940ea7650013ebd14751d10d672
Aprajit Kumar <aprajit.k@gmail.com>
2021-08-02 17:01:24 +0000

    completed the exercise

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  let total = 0;
+  if(fromN === toN){
+    return toN;
+  };
+  total = fromN + sum(fromN + 1, toN);     
+  return total
 }
 
 module.exports = sum;
